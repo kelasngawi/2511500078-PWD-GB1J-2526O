f1bd11f5647fbb8bf5afd06054b71d5d55e287f7
ade putra <2511500078@atmaluhur.ac.id>
2025-11-13 17:03:04 +0700

    update  pertemuan 08

--- pertemuan-07/index.php
+++ pertemuan-07/index.php
@@ -1,9 +1,23 @@
 <?php
 session_start();
-  $sesnama = $_SESSION["sesnama"] ; 
+  $sesname = $_SESSION["sesname"] ; 
   $sesemail = $_SESSION["sesemail"];
   $sespesan = $_SESSION["sespesan"];
- echo $sesnama . $sesemail . $sespesan;  
+ echo $sesname . $sesemail . $sespesan;  
+    ?>
+  
+  <?php
+  $sesnim = $_SESSION["sesNIM"];
+    $sesnama = $_SESSION["sesNama"];
+    $sestempat_lahir = $_SESSION["Tempat_lahir"];
+    $sestanggal_lahir = $_SESSION["Tanggal_lahir"];
+    $seshobi = $_SESSION["sesHobi"];
+    $sespasangan = $_SESSION["sesPasangan"];
+    $sespekerjaan = $_SESSION["sesPekerjaan"];
+    $sesnama_ortu = $_SESSION["sesNama_Ortu"];
+    $sesnama_kakak = $_SESSION["sesNama_Kakak"];
+    $sesnama_adik = $_SESSION["sesNama_Adik"];
+  echo $sesnim . $sesnama . $sestempat_lahir . $sestanggal_lahir . $seshobi . $sespasangan . $sespekerjaan . $sesnama_ortu . $sesnama_kakak . $sesnama_adik;
 ?>
 
 <!DOCTYPE html>
@@ -69,12 +83,90 @@ session_start();
       <p><strong>Nama Adik:</strong> Christina Setiawan</p>
     </section>
 
+    <section id="Entry Data Mahasiswa">
+      <h2>Data Mahasiswa</h2>
+      <form action="proses.php" method="POST">
+        <label for="txtNIM"><span>NIM:</span>
+          <input type="text" id="txtNIM" name="txtNIM" placeholder="Masukkan NIM" required autocomplete="nim">
+        </label>
+
+        <label for="txtNama"><span>Nama Lengkap:</span>
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama lengkap" required autocomplete="name">
+        </label>
+
+        <label for="txtTempat_lahir"><span>Tempat Lahir:</span>
+          <input type="text" id="txtTempat_lahir" name="txtTempat_lahir" placeholder="Masukkan tempat lahir" required autocomplete="tempat_lahir">
+        </label>
+
+        <label for="txtTanggal_lahir"><span>Tanggal Lahir:</span>
+          <input type="text" id="txtTanggal_lahir" name="txtTanggal_lahir" placeholder="Masukkan tanggal lahir" required autocomplete="tanggal_lahir">
+        </label>
+
+        <label for="txtHobi"><span>Hobi:</span>
+          <input type="text" id="txtHobi" name="txtHobi" placeholder="Masukkan Hobi" required autocomplete="hobi">
+        </label>
+
+        <label for="txtPasangan"><span>Pasangan:</span>
+          <input type="text" id="txtPasangan" name="txtPasangan" placeholder="Masukkan Pasangan" required autocomplete="pasangan">
+        </label>
+
+        <label for="txtPekerjaan"><span>Pekerjaan:</span>
+          <input type="text" id="txtPekerjaan" name="txtPekerjaan" placeholder="Masukkan Pekerjaan" required autocomplete="pekerjaan">
+        </label>
+
+        <label for="txtNama_Ortu"><span>Nama Orang Tua:</span>
+          <input type="text" id="txtNama_Ortu" name="txtNama_Ortu" placeholder="Masukkan Nama Orang Tua" required autocomplete="nama_ortu">
+        </label>
+
+        <label for="txtNama_Kakak"><span>Nama Kakak:</span>
+          <input type="text" id="txtNama_Kakak" name="txtNama_Kakak" placeholder="Masukkan Nama Kakak" required autocomplete="nama_kakak">
+        </label>
+
+        <label for="txtNama_Adik"><span>Nama Adik:</span>
+          <input type="text" id="txtNama_Adik" name="txtNama_Adik" placeholder="Masukkan Nama Adik" required autocomplete="nama_adik">
+        </label>
+
+        <button type="submit">Kirim</button>
+        <button type="reset">Batal</button>
+      </form>
+      <h2>Data yang diinputkan</h2>
+      <p>
+        NIM : <strong><?php echo $sesnim ?></strong> 
+      </p>
+      <p>
+        Nama : <strong><?php echo $sesnama ?></strong>
+      </p>
+      Tempat lahir : <strong><?php echo $sestempat_lahir ?></strong>
+      </p>
+      <p>
+        Tanggal lahir : <strong><?php echo $sestanggal_lahir ?></strong>
+      </p>
+      <p>
+        Hobi : <strong><?php echo $seshobi ?></strong>
+      </p>
+      <p>
+        Pasangan : <strong><?php echo $sespasangan ?></strong>
+      </p>
+      <p>
+        Pekerjaan : <strong><?php echo $sespekerjaan ?></strong>
+      </p>
+      <p>
+        Nama Orang Tua : <strong><?php echo $sesnama_ortu ?></strong>
+      </p>
+      <p>
+        Nama Kakak : <strong><?php echo $sesnama_kakak ?></strong>
+      </p>
+      <p>
+        Nama Adik : <strong><?php echo $sesnama_adik ?></strong>
+      </p>
+    </section>
+
     <section id="contact">
       <h2>Kontak Kami</h2>
       <form action="proses.php" method="POST">
 
-        <label for="txtNama"><span>Nama:</span>
-          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
+        <label for="txtName"><span>Name:</span>
+          <input type="text" id="txtName" name="txtName" placeholder="Masukkan name" required autocomplete="name">
         </label>
 
         <label for="txtEmail"><span>Email:</span>
@@ -92,7 +184,7 @@ session_start();
       </form>
       <h2>yang menghubungi kami</h2>
       <p>
-        Nama : <strong><?php echo $sesnama ?></strong> 
+        Nama : <strong><?php echo $sesname ?></strong> 
       </p>
       <p>
         Email : <strong><?php echo $sesemail ?></strong>
@@ -100,7 +192,7 @@ session_start();
       <p>
         Pesan : <strong><?php echo $sespesan ?></strong>
       </p>
-      
+
     </section>
   </main>
 
--- pertemuan-08/README.md
+++ pertemuan-08/README.md
@@ -1,1 +1,16 @@
-# pertemuan-08
+# pertemuan-07
+Hari ini , kamis 30 oktober 2025, saya<br>
+
+NIM : 2511500078<br>
+NAMA : Ade putra<br>
+<br>
+Hari Ini Kmais 6 November belajar sebagai berikut:
+<ol>
+  <li>download+extract pertemuan 07</li>
+  <li>method dan action index.php</li>
+  <li>buat file proses.php<li>
+  <li>ambil nilai input dari form ke proses.php<li>
+  <li>menyimpan nilai dari input ke variabel<li>
+  <li>session proses.php dan index.php<li>
+  <li>redirect ke index.php<li>
+</ol>
