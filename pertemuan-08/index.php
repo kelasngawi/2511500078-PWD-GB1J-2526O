24362d3f780191451d69d6506365d64695d5290f
ade putra <2511500078@atmaluhur.ac.id>
2025-11-13 17:22:24 +0700

    update  pertemuan 08

--- pertemuan-08/index.php
+++ pertemuan-08/index.php
@@ -5,6 +5,19 @@ session_start();
   $sespesan = $_SESSION["sespesan"];
  echo $sesname . $sesemail . $sespesan;  
     ?>
+  <?php
+  $sesnim = $_SESSION["sesNIM"];
+  $sesnama = $_SESSION["sesNama"];
+  $sestempat_lahir = $_SESSION["sesTempat_Lahir"];
+  $sestanggal_lahir = $_SESSION["sesTanggal_Lahir"];
+  $seshobi = $_SESSION["sesHobi"];
+  $sespasangan = $_SESSION["sesPasangan"];
+  $sespekerjaan = $_SESSION["sesPekerjaan"];
+  $sesnama_ortu = $_SESSION["sesNama_Ortu"];
+  $sesnama_kakak = $_SESSION["sesNama_Kakak"];
+  $sesnama_adik = $_SESSION["sesNama_Adik"];
+  echo $sesnim . $sesnama . $sestempat_lahir . $sestanggal_lahir . $seshobi . $sespasangan . $sespekerjaan . $sesnama_ortu . $sesnama_kakak . $sesnama_adik;
+?>
   
   
 <!DOCTYPE html>
@@ -42,6 +55,84 @@ session_start();
       <p>Ini contoh paragraf HTML.</p>
     </section>
 
+    <section id="Entry Data Mahasiswa"> 
+      <h2>Entry Data Mahasiswa</h2>
+      <form action="proses.php" method="POST">
+
+        <label for="txtNIM"><span>NIM:</span>
+          <input type="text" id="txtNIM" name="txtNIM" placeholder="Masukkan NIM" required autocomplete="nim">
+        </label>
+
+        <label for="txtNama"><span>Nama Lengkap:</span>
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan Nama Lengkap" required autocomplete="name">
+        </label>
+
+        <label for="txtTempat_lahir"><span>Tempat Lahir:</span>
+          <input type="text" id="txtTempat_lahir" name="txtTempat_lahir" placeholder="Masukkan Tempat Lahir" required autocomplete="birthplace">
+        </label>
+
+        <label for="txtTanggal_lahir"><span>Tanggal Lahir:</span>
+          <input type="date" id="txtTanggal_lahir" name="txtTanggal_lahir" placeholder="Masukkan Tanggal Lahir" required autocomplete="bday">
+        </label>
+
+        <label for="txtHobi"><span>Hobi:</span>
+          <input type="text" id="txtHobi" name="txtHobi" placeholder="Masukkan Hobi" required autocomplete="hobby">
+        </label>
+
+        <label for="txtPasangan"><span>Pasangan:</span>
+          <input type="text" id="txtPasangan" name="txtPasangan" placeholder="Masukkan Pasangan" required autocomplete="partner">
+        </label>
+
+        <label for="txtPekerjaan"><span>Pekerjaan:</span>
+          <input type="text" id="txtPekerjaan" name="txtPekerjaan" placeholder="Masukkan Pekerjaan" required autocomplete="occupation">
+        </label>
+
+        <label for="txtNama_Ortu"><span>Nama Orang Tua:</span>
+          <input type="text" id="txtNama_Ortu" name="txtNama_Ortu" placeholder="Masukkan Nama Orang Tua" required autocomplete="parent's name">
+        </label>
+
+        <label for="txtNama_Kakak"><span>Nama Kakak:</span>
+          <input type="text" id="txtNama_Kakak" name="txtNama_Kakak" placeholder="Masukkan Nama Kakak" required autocomplete="sibling's name">
+        </label>
+        <label for="txtNama_Adik"><span>Nama Adik:</span>
+          <input type="text" id="txtNama_Adik" name="txtNama_Adik" placeholder="Masukkan Nama Adik" required autocomplete="sibling's name">
+        </label>
+        <button type="submit">Kirim</button>
+        <button type="reset">Batal</button>
+      </form>
+      <h2>Data yang diinputkan</h2>
+      <p>
+        NIM : <strong><?php echo $sesnim ?></strong>
+      </p>
+      <p>
+        Nama : <strong><?php echo $sesnama ?></strong>
+      </p>
+      <p>
+        Tempat lahir : <strong><?php echo $sestempat_lahir ?></strong>
+      </p>
+      <p>
+        Tanggal lahir : <strong><?php echo $sestanggal_lahir ?></strong>
+      </p>
+      <p>
+        Hobi : <strong><?php echo $seshobi ?></strong>
+      </p>
+      <p>
+        Pasangan : <strong><?php echo $sespasangan ?></strong>
+      </p>
+      <p>
+        Pekerjaan : <strong><?php echo $sespekerjaan ?></strong>
+      </p>
+      <p>
+        Nama Orang Tua : <strong><?php echo $sesnama_ortu ?></strong>
+      </p>
+      <p>
+        Nama Kakak : <strong><?php echo $sesnama_kakak ?></strong>
+      </p>
+      <p>
+        Nama Adik : <strong><?php echo $sesnama_adik ?></strong>
+      </p>
+
+    </section>
 
     <section id="about">
       <?php
