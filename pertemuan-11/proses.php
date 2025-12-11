bf89487ab69b430e7a224e65fed46d26498389f3
ade putra <2511500078@atmaluhur.ac.id>
2025-12-11 18:11:46 +0700

    update  pertemuan 11

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -47,7 +47,31 @@ if (!empty($errors)) {
   redirect_ke('index.php#contact');
 }
 
+$sql = "INSERT INTO tbl_tamu (cnama, cemail, cpesan) VALUES (?, ?, ?)";
+$stmt = mysqli_prepare($conn, $sql);
 
+if ($stmt === false) {
+  $_SESSION['flash_error'] = 'terjadi kesalahan sistem (prepare gagal).';
+  redirect_ke('index.php#contact');
+}
+
+mysqli_stmt_bind_param($stmt, "sss", $nama, $email, $pesan);
+
+if (mysqli_stmt_execute($stmt)) {
+  unset($_SESSION['old']);
+  $_SESSION['flash_sukses'] = 'Terima kasih, data anda telah tersimpan.';
+  redirect_ke('index.php#contact');
+} else {
+  $_SESSION['old'] = [
+    'nama' => $nama,
+    'email' => $email,
+    'pesan' => $pesan,
+  ];
+  $_SESSION['flash_error'] = 'Data gagal tersimpan. silahkan coba lagi.';
+  redirect_ke('index.php#contact');
+}
+
+mysqli_stmt_close($stmt);
 
 $arrBiodata = [
   "nim" => $_POST["txtNim"] ?? "",
