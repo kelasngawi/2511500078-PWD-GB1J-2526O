47ac764e3ec35a58475c672bd2feae295fb24a18
ade putra <2511500078@atmaluhur.ac.id>
2025-12-11 17:56:39 +0700

    update  pertemuan 11

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -15,6 +15,40 @@ $email = bersihkan($_POST["txtEmail"] ?? "");
 $pesan = bersihkan($_POST["txtPesan"] ?? "");
 $_SESSION["contact"] = $arrContact;
 
+$errors = [];
+
+if ($nama === '') {
+  $errors[] = 'Nama wajib di isi.';
+}
+
+if ($email === '') {
+  $errors[] = 'Email wajib di isi.';
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $errors[] = 'Format e-mail tidak valid.';
+}
+
+if ($pesan === '') {
+  $errors[] = 'Pesan wajib di isi.';
+}
+
+
+
+
+
+
+if (!empty($errors)) {
+  $_SESSION['old'] = [
+    'nama' => $nama,
+    'email' => $email,
+    'pesan' => $pesan,
+  ];
+
+  $_SESSION['flash_error'] = implode('<br>', $errors);
+  redirect_ke('index.php#contact');
+}
+
+
+
 $arrBiodata = [
   "nim" => $_POST["txtNim"] ?? "",
   "nama" => $_POST["txtNmLengkap"] ?? "",
